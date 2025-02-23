116af0c19b5a5f1ac8bc02109b7b1d49641c7388
Peter Chua-Lao <peter.chua-lao@primecomputinglab.com>
2025-02-23 12:06:28 +1000

    Initial Commit

---
+++ TideTimes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TideTimes
+//
+//  Created by Peter Chua-Lao on 23/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TideTimes/TideTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesApp.swift
+//  TideTimes
+//
+//  Created by Peter Chua-Lao on 23/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct TideTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
